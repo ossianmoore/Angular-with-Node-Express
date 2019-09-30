1039e2ff856cfed324cddba9eafe2d4f44e77d1a
Ossian Moore <43648387+ossianmoore@users.noreply.github.com>
2019-09-30 11:49:07 +0100

    commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MovieServiceService } from './Services/movie-service.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'movie-app';
+  MyMovies:any = [];
+
+  constructor(private movieService:MovieServiceService){
+this.movieService.GetMovieInformation().subscribe((data)=>{
+    this.MyMovies = data.Search;
+    console.log(this.MyMovies);
+  })
+  
+
+}
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+MatInputModule,
+MatCardModule,
+MatButtonModule,
+MatToolbarModule,
+MatExpansionModule
+} from '@angular/material';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
